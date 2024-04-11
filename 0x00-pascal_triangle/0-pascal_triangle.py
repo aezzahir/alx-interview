8fa33f796b351347de2d99b2208a920245c7acc3
aezzahir <aero.glowsky@gmail.com>
2024-04-11 14:42:05 +0000

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
+
+def pascal_triangle(N):
+
+    C = [[1]]
+    for n in range(1, N):
+        B = [1]
+        for m in range(1, n):
+            B.append(C[n - 1][m] + C[n - 1][m - 1])
+        B.append(1)
+        C.append(B)
+    return C
