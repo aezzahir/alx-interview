771cefa8d44f003269e4d149e18c336f9d4329ed
aezzahir <aero.glowsky@gmail.com>
2024-05-28 20:27:58 +0100

    [200~Given an n x n 2D matrix, rotate it 90 degrees clockwise.~

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,6 +2,7 @@
 """Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
 
 def rotate_2d_matrix(matrix):
+    """Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
     n = len(matrix)
     for i in range(n // 2):
         for j in range(i, n - i - 1):
